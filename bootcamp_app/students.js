ab109957a5c58067936c6aad446ca39e8f382aa2
Seamus McGill <seamusmcgill@gmail.com>
2022-02-09 18:26:41 -0500

    Prevent SQL injection with paramaterized query

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,13 +7,17 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const queryString = `
+  SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort
+  FROM students JOIN cohorts on students.cohort_id = cohorts.id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2
+`;
+const cohortName = process.argv[2];
+const limit = process.argv[3];
+const values = [`%${cohortName}%`, limit];
 
-pool.query(`
-SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort
-FROM students JOIN cohorts on students.cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+pool.query(queryString, values)
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,15 +7,21 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const queryString = `
   SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
   FROM teachers 
   JOIN assistance_requests ON teachers.id = assistance_requests.teacher_id
   JOIN students on students.id = assistance_requests.student_id
   JOIN cohorts on students.cohort_id = cohorts.id
-  WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-  ORDER BY teacher;
-`)
+  WHERE cohorts.name LIKE $1
+  ORDER BY teacher
+  LIMIT $2;
+`;
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
+
+pool.query(queryString, values)
   .then(res => {
     res.rows.forEach(row => {
       console.log(`${row.cohort}: ${row.teacher}`);
